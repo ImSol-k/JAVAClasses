da067a0748a0c76d8cb17e1640e45d90e4fd53b6
tiq0015 <tiq0015@naver.com>
2023-12-26 15:20:42 +0900

    2023.12.26 조건문

--- src/com/javaex/ex02/Ex01.java
+++ src/com/javaex/ex02/Ex01.java
@@ -3,53 +3,50 @@ package com.javaex.ex02;
 public class Ex01 {
 
 	public static void main(String[] args) {
-		
-		//print, println의 차이 - 개행여부
+
+		// print, println의 차이 - 개행여부
 		String str = "안녕하세요";
 		String name = "소리";
 		String hello = "굿모닝";
 		String s = "한";
 		char c = '한';
 		int i = 12345;
-		double d = 3.14;		
-		
+		double d = 3.14;
+
 		System.out.println(i);
-		System.out.println(12345);		
+		System.out.println(12345);
 		System.out.println(str);
 		System.out.println("안녕하세요");
-		
+
 		System.out.print(name);
 		System.out.println(str);
-		
+
 		System.out.print("안녕");
 		System.out.println("하세요");
 		System.out.println(name + "님 " + hello + hello);
-		System.out.println(name + '님' +  ' ' + hello + hello);		
+		System.out.println(name + '님' + ' ' + hello + hello);
 		System.out.println("제 이름은 " + name + "입니다.");
 		System.out.println("================================");
-		
+
 		System.out.println(i + i);
 		System.out.println(d + d);
 		System.out.println(i + d);
 		System.out.println(c + c);
 		System.out.println(s + s);
-		//문자열이 들어가면 전부 문자열로 인식
+		// 문자열이 들어가면 전부 문자열로 인식
 		System.out.println(name + i + d);
-		//숫자먼저 계산해야 계산식으로 들어감
+		// 숫자먼저 계산해야 계산식으로 들어감
 		System.out.println(name + (i + d));
 		System.out.println("================================");
-		
-		//이스케이프 시퀀스
-		//문자열 안에서 특별한 의미로 해석되는 문자
+
+		// 이스케이프 시퀀스
+		// 문자열 안에서 특별한 의미로 해석되는 문자
 		// \n(개행), \t(탭), \"(큰따옴표), \\(역슬래쉬)
-		//문자열은 큰따옴표사용"" 문자형은 작은따옴표사용''하기떄문에
-		//각 따옴표는 역슬래쉬 붙여서 사용해야됨
+		// 문자열은 큰따옴표사용"" 문자형은 작은따옴표사용''하기떄문에
+		// 각 따옴표는 역슬래쉬 붙여서 사용해야됨
 		System.out.print("안녕 나는 \"소리\"야 \\^O^/\t(<''<)" + '\'' + '"' + '\n');
 		System.out.println("================================");
-		
-		
-		
-		
+
 	}
 
 }
--- src/com/javaex/ex02/Ex02.java
+++ src/com/javaex/ex02/Ex02.java
@@ -1,39 +1,36 @@
 package com.javaex.ex02;
 
 //import - 외부에있는 클래스 불러오기
-import java.util.Scanner;	//Scanner참조
+import java.util.Scanner; //Scanner참조
 
 public class Ex02 {
 
 	public static void main(String[] args) {
-		
-		//Scanner(콘솔입력)
-		//사용자입력 받아오는 클래스
-		//참조형! 변수에 값이 아닌 주소를 저장함
-		//참조형은 클래스로 정의되어있기떄문에 변수선언할때도 잠조자료형은 대문자로 시작한다.		
-		//System.in - 키보드 입력 받아오는 클래스
-		Scanner sc = new Scanner(System.in); 
+
+		// Scanner(콘솔입력)
+		// 사용자입력 받아오는 클래스
+		// 참조형! 변수에 값이 아닌 주소를 저장함
+		// 참조형은 클래스로 정의되어있기떄문에 변수선언할때도 잠조자료형은 대문자로 시작한다.
+		// System.in - 키보드 입력 받아오는 클래스
+		Scanner sc = new Scanner(System.in);
 		String name;
 		int age;
-		
-		//출력
+
+		// 출력
 		System.out.println("=================================");
 		System.out.println("이름을 입력해주세요.");
 		System.out.print("이름 : ");
-		//입력
+		// 입력
 		name = sc.nextLine();
-		//출력
+		// 출력
 		System.out.println("나이를 입력해주세요");
 		System.out.print("나이 : ");
-		//입력
+		// 입력
 		age = sc.nextInt();
-		//결과출력
-		System.out.println("당신의 이름은 " + name + " 나이는 " + age + "살 입니다.");		
+		// 결과출력
+		System.out.println("당신의 이름은 " + name + " 나이는 " + age + "살 입니다.");
 		System.out.println("=================================");
-		sc.close();		//세트로 항상 달아주기
-		
-		
-		
+		sc.close(); // 세트로 항상 달아주기
 
 	}
 
