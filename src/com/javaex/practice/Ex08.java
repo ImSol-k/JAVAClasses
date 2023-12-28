ff6a7d17a1a61d28d46e05c48859c4a7676c9414
tiq0015 <tiq0015@naver.com>
2023-12-28 21:52:51 +0900

    parctice03

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -4,16 +4,14 @@ public class Ex08 {
 
 	public static void main(String[] args) {
 		
-		int dan = 2;
 		int num = 1;
 		int result;
 		
-		for(int i = 1; i < 9; i++) {
-			//System.out.print(i);
-			result = dan * i;
-			//System.out.print(num + " * " + i + " = " + result + "\t");
-			for(int j = 1; j <= 9; j++) {				
-				System.out.print(dan + " * " + num + " = " + result + "\t");
+		for(int i = 1; i <= 9; i++) {
+			
+			for(int j = 2; j <= 9; j++) {
+				result = j * i;
+				System.out.print(j + " * " + num + " = " + result + "\t");
 			}
 			num++;
 			System.out.println();
