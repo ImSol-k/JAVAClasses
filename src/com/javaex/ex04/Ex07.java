c8c00cba1fb0426ed4cf9d55d8f7036176e878d0
tiq0015 <tiq0015@naver.com>
2023-12-28 13:06:45 +0900

    중첩for문 활용

--- src/com/javaex/ex04/Ex07.java
+++ src/com/javaex/ex04/Ex07.java
@@ -1,28 +1,36 @@
 package com.javaex.ex04;
 
+import java.util.Scanner;
+
 public class Ex07 {
 
 	public static void main(String[] args) {
-				
+		
+		Scanner in = new Scanner(System.in);
 		//2단부터 시작
-		int dan=2;
+		int dan1, dan2;
 		int sum;
 		
-		//총 9단까지 출력한다
-		for(int i = 1; i < 9; i++) {
+		System.out.print("몇단부터 출력? : ");
+		dan1 = in.nextInt();
+		System.out.print("몇단까지 출력? : ");
+		dan2 = in.nextInt();
+		
+		//총 dan2단까지 출력한다
+		for(int i = dan1; i <= dan2; i++) {
 			
 			//곱셈연산하는 for문
 			for(int j = 1; j <= 9; j++) {
-			sum = dan * j;
-			System.out.println(dan + " * " + j + " = " + sum);			
+			sum = dan1 * j;
+			System.out.println(dan1 + " * " + j + " = " + sum);			
 			}
 			
-			//한 단이 끝날때마다 단+1, 개행
-			dan++;
+			//한 단이 끝날때마다 dan1+1, 개행
+			dan1++;
 			System.out.println();
 		}
 		
-		
+		in.close();
 	}
 
 }
