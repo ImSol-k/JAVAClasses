c5039703c45c7faf1201c33862a32a88b4ab8e27
tiq0015 <tiq0015@naver.com>
2023-12-28 12:21:06 +0900

    for문 활용

--- src/com/javaex/ex04/Ex06.java
+++ src/com/javaex/ex04/Ex06.java
@@ -2,11 +2,10 @@ package com.javaex.ex04;
 
 import java.util.Scanner;
 
-public class Ex05 {
+public class Ex06 {
 
 	public static void main(String[] args) {
 		
-		//1부터 10까지의 합을 구하는 프로그램 작성
 		Scanner in = new Scanner(System.in);
 		int sum = 0;
 		int num;
@@ -14,16 +13,14 @@ public class Ex05 {
 		System.out.print("합할 정수 입력 : ");
 		num = in.nextInt();
 		
-		
 		for(int i = 1; i <= num; i++) {
 			
 			sum = i + sum;
-						
+			
+			System.out.println(i + "까지의 합은" + sum);
 		}
 		
-		System.out.println("1부터 " + num + "까지의 정수의 합은 " + sum + "입니다.");
 		
-		in.close();
 		
 	}
 
