35d06e6130affcadb7b935d7e3c40bc8897d5225
tiq0015 <tiq0015@naver.com>
2023-12-28 15:21:16 +0900

    반복문 연습

---
+++ src/com/javaex/ex04/Ex11.java
@@ -0,0 +1,24 @@
+package com.javaex.ex04;
+
+public class Ex11 {
+
+	public static void main(String[] args) {
+		
+		//1에서 20까지의 수에서 2의배수, 3의배수를 제외한 숫자 출력
+		//for문, countinue사용
+		
+		int num = 20;
+		
+		for(int i = 1; i <= num; i++) {
+			if(i % 2 == 0) {
+				continue;
+			}
+			else if(i % 3 == 0) {
+				continue;
+			}
+			System.out.println(i);
+		}		
+		
+	}
+
+}
