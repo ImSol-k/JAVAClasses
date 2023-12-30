289def309db179caf2441f35340875d22bb2b6f6
tiq0015 <tiq0015@naver.com>
2023-12-30 11:30:05 +0900

    로또만들기

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -3,37 +3,26 @@ package com.javaex.practice;
 public class Ex08 {
 
 	public static void main(String[] args) {
-		
-		//배열을 사용해서 1~45까지의 숫자중 랜덤숫자 6개 출력
-		//중복체크하기
-		
+
+		// 배열을 사용해서 1~45까지의 숫자중 랜덤숫자 6개 출력
+		// 중복체크하기
+
+		//배열선언
 		int[] num = new int[6];
-		boolean bl = true;
-		
-		do {
-			for(int i = 0; i < num.length; i++) {
-				num[i] = (int)(Math.random() * 7) + 1;
-				for(int j = 0; j > i; j++) {
-					if(num[i] == num[j]) {
-						bl = true;
-					}
-					else {
-						bl = false;
-					}
+
+		//랜덤한 숫자 생성
+		for (int i = 0; i < num.length; i++) {
+			num[i] = (int) (Math.random() * 7) + 1;
+
+			//중복체크
+			for (int j = 0; j < i; j++) {
+				//num[i]와 num[j]가 같으면
+				if (num[i] == num[j]) {
+					i--;
 				}
 			}
-		}while(bl);
-		
-		
-//		for(int i = 0; i < num.length; i++) {
-//			num[i] = (int)(Math.random() * 7) + 1;
-//			for(int j = 0; j < i; j++) {
-//				if(num[i] == num[j]) {
-//					
-//				}
-//			}
-//		}
-		for(int i = 0; i > num.length; i++) {
+		}
+		for (int i = 0; i < num.length; i++) {
 			System.out.print(num[i] + " ");
 		}
 	}
