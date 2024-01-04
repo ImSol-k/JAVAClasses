62ee15ff84dffb0c9df5e6afaf39c3e4848347d9
tiq0015 <tiq0015@naver.com>
2024-01-04 10:13:51 +0900

    2024.01.04 GetterSetter, toString 단축키 활용

--- src/com/javaex/ex05/Song.java
+++ src/com/javaex/ex05/Song.java
@@ -1,29 +1,84 @@
 package com.javaex.ex05;
 
 public class Song {
-	
+
 	private String title;
 	private String artist;
 	private String album;
 	private String composer;
 	private int track;
 	private int year;
-	
-	public void setSong(String artist, String title) {
-		this.artist = artist;
-		this.title = title;
-		
+
+	public String getTitle() {
+		return title;
 	}
-	public void setAlbum(String album, int year, int track, String composer) {
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(String album) {
 		this.album = album;
-		this.year = year;
-		this.track = track;
+	}
+
+	public String getComposer() {
+		return composer;
+	}
+
+	public void setComposer(String composer) {
 		this.composer = composer;
 	}
+
+	public int getTrack() {
+		return track;
+	}
+
+	public void setTrack(int track) {
+		this.track = track;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	/*
+	 * public void setSong(String artist, String title) { 
+	 * this.artist = artist;
+	 * this.title = title;
+	 * 
+	 * } public void setAlbum(String album, int year, int track, String composer) {
+	 * this.album = album; 
+	 * this.year = year; 
+	 * this.track = track; 
+	 * this.composer = composer;
+	 * }
+	 */
 	
 	public void showInfo() {
-		System.out.println(artist + ", " + title + "(" + album + ", " + year + ", "+ track + "번 track, "+ composer + " 작곡)");
+		System.out.println(
+				artist + ", " + title + "(" + album + ", " + year + ", " + track + "번 track, " + composer + " 작곡)");
 	}
-	
-	
+	//검사용 문자열
+	@Override
+	public String toString() {
+		return "Song [title=" + title + ", artist=" + artist + ", album=" + album + ", composer=" + composer
+				+ ", track=" + track + ", year=" + year + "]";
+	}
+
 }
--- src/com/javaex/ex05/SongApp.java
+++ src/com/javaex/ex05/SongApp.java
@@ -1,23 +1,45 @@
 package com.javaex.ex05;
 
 public class SongApp {
-	
+
 	public static void main(String[] args) {
-		
-		Song so1 = new Song();
-		so1.setSong("아이유", "좋은날");
-		so1.setAlbum("Real", 2010, 3, "이민수");
+
+		Song so1 = new Song();so1.setArtist("아이유");
+		so1.setTitle("좋은날");
+		so1.setAlbum("Real");
+		so1.setYear(2010);
+		so1.setTrack(3);
+		so1.setComposer("이민수");
+//		so1.setSong("아이유", "좋은날");
+//		so1.setAlbum("Real", 2010, 3, "이민수");
 		
 		Song so2 = new Song();
-		so2.setSong("BIGBANG", "거짓말");
-		so2.setAlbum("Always", 2007, 2, "G-DRAGON");
+		so2.setArtist("BIGBANG");
+		so2.setTitle("거짓말");
+		so2.setAlbum("Always");
+		so2.setYear(2007);
+		so2.setTrack(2);
+		so2.setComposer("G-DRAGON");
+//		so2.setSong("BIGBANG", "거짓말");
+//		so2.setAlbum("Always", 2007, 2, "G-DRAGON");
 		
 		Song so3 = new Song();
-		so3.setSong("버스커버스커", "벚꽃엔딩");
-		so3.setAlbum("버스커버스커1집", 2012, 4, "장범준");
+		so3.setArtist("버스커버스커");
+		so3.setTitle("벚꽃엔딩");
+		so3.setAlbum("버스커버스커1집");
+		so3.setYear(2012);
+		so3.setTrack(4);
+		so3.setComposer("장범준");
+//		so3.setSong("버스커버스커", "벚꽃엔딩");
+//		so3.setAlbum("버스커버스커1집", 2012, 4, "장범준");
 		
-		so1.showInfo();
-		so2.showInfo();
-		so3.showInfo();
+		// 검사용 toString메소드로 확인하는 방법
+		System.out.println(so1.toString());
+		System.out.println(so2.toString());
+		System.out.println(so3.toString());
+//		so1.showInfo();
+//		so2.showInfo();
+//		so3.showInfo();
+
 	}
 }
