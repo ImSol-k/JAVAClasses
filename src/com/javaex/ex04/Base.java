9ccb44fa9026439427b4a6cf38648821e681b7ec
tiq0015 <tiq0015@naver.com>
2024-01-06 13:11:22 +0900

    제출

--- src/com/javaex/ex04/Base.java
+++ src/com/javaex/ex04/Base.java
@@ -5,25 +5,33 @@ public class Base {
 	private static String state;
     
     public void service(String state) {
+    	
     	this.state = state;
-    	if
-    	day();
-    	night();
-    	afternoon();
+    	
+    	if(state.equals("낮")) {
+    		day();
+    	}
+    	else if(state.equals("밤")) {
+    		night();
+    	}
+    	else if(state.equals("오후")){
+    		afternoon();
+    	}
+    	
     	
     }
 
     public void day() {
-        System.out.println("낮에는 열심히 수업듣자");
+        System.out.println(state + "에는 열심히 수업듣자");
     }
 
     public void night() {
         
-    	System.out.println("밤에는 숙면");
+    	System.out.println(state + "에는 숙면");
     }
     
     public void afternoon(){
-        System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
+        System.out.println(state + "도 낮과 마찬가지로 공부해야 합니다.");
     }
 }
 
