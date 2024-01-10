8be014cf078613aba177c648934bd24f75cd9dd9
tiq0015 <tiq0015@naver.com>
2024-01-10 17:32:50 +0900

    2024.01.10 과제

---
+++ src/com/javaex/ex02/Bird.java
@@ -0,0 +1,27 @@
+package com.javaex.ex02;
+
+public abstract class Bird {
+	
+    private String name;
+
+    
+    public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void sing() {
+		System.out.println();
+	}
+    public void fly() {
+    	System.out.println();
+    }
+    public void showName() {
+    	System.out.println();
+    }
+
+
+
+}
