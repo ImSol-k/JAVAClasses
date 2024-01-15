75a7996fc23503219ba668de56606cbfc3beca1a
tiq0015 <tiq0015@naver.com>
2024-01-15 11:37:19 +0900

    Set이용해서 로또번호 출력(중복체크)

--- src/com/javaex/ex06/Ex01.java
+++ src/com/javaex/ex06/Ex01.java
@@ -8,22 +8,16 @@ public class Ex01 {
 	public static void main(String[] args) {
 				
 		Set<Integer> iSet = new HashSet<Integer>();
-		int a = 0;
 		
-//		for (int i = 0; i < 6; i++) {
-//			iSet.add((int)(Math.random() * 45) + 1);
-//		}
 		while(iSet.size() < 6) {
 			iSet.add((int)(Math.random() * 45) + 1);
 			
 		}
+		
 		for(Integer i : iSet) {
 			System.out.print(i.toString() + "  ");
 		}
 		System.out.println("\n======================");
-		
-		
-		
 		
 	}
 
